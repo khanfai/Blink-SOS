8e44044ba517645e93aa5d067ff11347e57ef8e5
khanfai <khanfai@deakin.edu.au>
2017-09-19 12:26:39 +1000

    Add files via upload

---
+++ Motion.ino
@@ -0,0 +1,23 @@
+void setup() {
+  // put your setup code here, to run once:
+pinMode (LED_BUILTIN,OUTPUT);
+Serial.begin(9600);
+pinMode(6, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if ((digitalRead(6) == HIGH && digitalRead(5) == HIGH))
+  {
+    Serial.println("Active");
+    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+      
+
+  }
+  else {
+    Serial.println("Inactive");
+    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+                    
+  }
+  delay(1000);
+}
